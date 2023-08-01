49d722325e5f76a34a48f58143322db47eb13e5c
mostafa farrag <mostafa.kz@outlook.sa>
2023-08-01 08:44:55 +0200

    create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { CqrsModule } from '@nestjs/cqrs';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CqrsModule,
-
     PostModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
